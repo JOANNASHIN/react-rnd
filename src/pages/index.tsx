45a0de4f9b6554ec5e06696beaff48f0fa388d0f
JOANNASHIN <joannashin3@gmail.com>
2023-02-28 17:08:18 +0900

    getStaticProps test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,37 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
 import Layout, { siteTitle } from '@/components/layout';
-const inter = Inter({ subsets: ['latin'] })
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>홈 {siteTitle}</title>
       </Head>
 
-      <section className={utilStyles.headingMd}>
-        <p>안녕하세요, 신지은입니다.</p>
-        <p>
-          소개글
-        </p>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
