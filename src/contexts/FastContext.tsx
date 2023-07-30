1d2d7f62b44de43185e1aa44ef2e198aaf49ef2b
joannashin <joannashin3@gmail.com>
2023-07-30 23:23:20 +0900

    fast contexts 분리

---
+++ src/contexts/FastContext.tsx
@@ -0,0 +1,104 @@
+
+import React, { createContext, useCallback, useContext, useEffect, useRef, useState, useSyncExternalStore } from "react";
+
+
+export default function createFastContext<Store>(initialState: Store) {
+  //#region type
+
+  type StoreSet = (value: Partial<Store>) => void;
+  type UseStoreDataReturnType = ReturnType<typeof useStoreData>;
+  type TCallback = () => void;
+
+  interface UseStoreDataProps {
+    get: () => Store;
+    set: StoreSet;
+    subscribe: (callback: TCallback) => TCallback;
+  }
+  //#endregion
+    
+  /**
+   * 스토어 데이터
+   */
+  function useStoreData(): UseStoreDataProps {
+    const store = useRef(initialState)
+
+    const get = useCallback(() => store.current, []);
+
+    const subscribers = useRef(new Set<TCallback>());
+
+    const set = useCallback((value:  Partial<Store>) => {
+      store.current = {
+        ...store.current,
+        ...value
+      }
+
+      subscribers.current.forEach((callback) => callback());
+    }, []);
+    
+    
+
+    const subscribe = useCallback((callback: TCallback) => {
+      subscribers.current.add(callback);
+
+      return () => {
+        subscribers.current.delete(callback);
+      }
+    }, []);
+    
+    return {
+      get,
+      set,
+      subscribe,
+    };
+  }
+
+
+  /** 스토어 context */
+  const StoreContext = createContext<null | UseStoreDataReturnType>(null);
+
+  /** 스토어 context Provider 
+   * - 데이터는 useStoreData- get, set, subscribers 
+  */
+  function Provider ({children} : {children: React.ReactNode}) {
+    const store = useStoreData();
+
+    return (
+      <StoreContext.Provider value={store}>
+        {children}
+      </StoreContext.Provider>
+    )
+  }
+
+  /**
+   * 스토어 사용시에 부를 함수
+   */
+  function useStore<SelecterOutput>(
+    selector: (store: Store) => SelecterOutput
+  ): [
+    SelecterOutput, 
+    StoreSet
+  ] {
+    const store = useContext(StoreContext);
+    if (!store) {
+      throw new Error('Store not found');
+    }
+
+    // const [state, setState] = useState(store.get());
+
+    // useEffect(() => {
+    //   return store.subscribe(() => setState(store.get()))
+    // }, [])
+
+    const state = useSyncExternalStore(store.subscribe, () => selector(store.get()));
+    
+    return [
+      state,
+      store.set
+    ]
+  }
+
+  return {
+    Provider,
+    useStore,
+  }
+}
--- src/pages/reactFactContext.tsx
+++ src/pages/reactFactContext.tsx
@@ -2,108 +2,13 @@
  * 참고
  * https://www.youtube.com/watch?v=ZKlXqrcBx88
  */
-import React, { createContext, useCallback, useContext, useEffect, useRef, useState, useSyncExternalStore } from "react";
+import createFastContext from "@/contexts/FastContext";
+import React from "react";
 
-//#region type
-interface Store {
-  first: string;
-  last: string;
-}
-
-type StoreSet = (value: Partial<Store>) => void;
-type UseStoreDataReturnType = ReturnType<typeof useStoreData>;
-type TCallback = () => void;
-
-interface UseStoreDataProps {
-  get: () => Store;
-  set: StoreSet;
-  subscribe: (callback: TCallback) => TCallback;
-}
-//#endregion
-
-
-/**
- * 스토어 데이터
- */
-function useStoreData(): UseStoreDataProps {
-  const store = useRef({
-    first: "",
-    last: "",
-  })
-
-  const get = useCallback(() => store.current, []);
-
-  const subscribers = useRef(new Set<TCallback>());
-
-  const set = useCallback((value:  Partial<Store>) => {
-    store.current = {
-      ...store.current,
-      ...value
-    }
-
-    subscribers.current.forEach((callback) => callback());
-  }, []);
- 
-  
-
-  const subscribe = useCallback((callback: TCallback) => {
-    subscribers.current.add(callback);
-
-    return () => {
-      subscribers.current.delete(callback);
-    }
-  }, []);
-  
-  return {
-    get,
-    set,
-    subscribe,
-  };
-}
-
-
-/** 스토어 context */
-const StoreContext = createContext<null | UseStoreDataReturnType>(null);
-
-/** 스토어 context Provider 
- * - 데이터는 useStoreData- get, set, subscribers 
-*/
-function Provider ({children} : {children: React.ReactNode}) {
-  const store = useStoreData();
-
-  return (
-    <StoreContext.Provider value={store}>
-      {children}
-    </StoreContext.Provider>
-  )
-}
-/**
- * 스토어 사용시에 부를 함수
- */
-function useStore<SelecterOutput>(
-  selector: (store: Store) => SelecterOutput
-): [
-  SelecterOutput, 
-  StoreSet
-] {
-  const store = useContext(StoreContext);
-  if (!store) {
-    throw new Error('Store not found');
-  }
-
-  // const [state, setState] = useState(store.get());
-
-  // useEffect(() => {
-  //   return store.subscribe(() => setState(store.get()))
-  // }, [])
-
-  const state = useSyncExternalStore(store.subscribe, () => selector(store.get()));
-  
-  return [
-    state,
-    store.set
-  ]
-}
+const { Provider, useStore } = createFastContext({
+  first: "Joanna",
+  last: "Shin"
+})
 
 const TextInput = ({ value }: { value: "first" | "last" }) => {
   const [fieldValue, setStore] = useStore((store) => store[value]);
